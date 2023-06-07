f315d34514bd93463599e50f66b8d2a547349031
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-06-07 12:59:34 +0200

    new updates

--- src/pages/PressPage/index.tsx
+++ src/pages/PressPage/index.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Box,
-  styled,
-} from '@mui/material';
+import { Box, styled } from '@mui/material';
 import Section, { SectionTitle } from '../../components/common/section';
-import { NewsCard, pressPageData } from '../../__mocks__/pages/presspage';
+import { NewsCard } from '../../__mocks__/pages/presspage'; // Assuming `NewsCard` is exported from `presspageData.js`
 import { NoDataParagraph } from '../../components/common/paragraph';
 import LoadingComponent from '../../components/common/loading';
 import NewsCardComponent from './NewsCard';
-import Pagination from '../../components/common/pagination';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { pressPageData } from '../../__mocks__/pages/presspage';
 
 const PressPageWrapper = styled(Box)(({ theme }) => ({
   ...theme.bodyProps,
@@ -32,15 +30,15 @@ const NewsCards = styled(Box)(({ theme }) => ({
 }));
 
 const PressPage = () => {
-  const [newsData, setNewsData] = useState<NewsCard[]>([]);
+  const [newsData, setNewsData] = useState<NewsCard[]>([]); // Specify the type of `newsData` as `NewsCard[]`
   const [isLoading, setIsLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(1);
-  const [newsPerPage, setNewsPerPage] = useState(5); // Number of news to show per page
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { pressReleases }: { pressReleases: NewsCard[] } = await pressPageData.getPressReleases();
+        const { pressReleases }: { pressReleases: NewsCard[] } = await pressPageData.getPressReleases(); // Update the type of `pressReleases`
         setNewsData(pressReleases);
         setIsLoading(false);
       } catch (error) {
@@ -53,46 +51,54 @@ const PressPage = () => {
     fetchData();
   }, []);
 
-  const handleChangePage = (_event: React.ChangeEvent<unknown>, newPage: number) => {
-    setPage(newPage);
-  };
+  const fetchMoreNews = async () => {
+    try {
+      const newsPerPage = 10; // Number of news cards per page
+      const nextPage = page + 1;
+      const start = (nextPage - 1) * newsPerPage;
+      const end = nextPage * newsPerPage;
 
-  const handleChangeNewsPerPage = (event: { target: { value: any } }) => {
-    setNewsPerPage(event.target.value as number);
-    setPage(1); // Reset the page when changing news per page
+      const { pressReleases }: { pressReleases: NewsCard[] } = await pressPageData.getPressReleases(false, start, end);
+      if (pressReleases.length > 0) {
+        setNewsData((prevNewsData) => [...prevNewsData, ...pressReleases]);
+        setPage(nextPage);
+        setHasMore(true); // Set `hasMore` to true when there are more pages to fetch
+      } else {
+        setHasMore(false); // Set `hasMore` to false when there are no more pages to fetch
+      }
+    } catch (error) {
+      // Handle error here
+      console.error('Error fetching more news:', error);
+    }
   };
-
-  const startIndex = (page - 1) * newsPerPage;
-  const endIndex = startIndex + newsPerPage;
 
   return (
     <PressPageWrapper>
       <Section>
         <SectionTitle>Press Releases</SectionTitle>
-        {isLoading ? (
+        {isLoading && newsData.length === 0 ? (
           <NoDataParagraph>
             <LoadingComponent loaderType="box" />
           </NoDataParagraph>
-        ) : newsData && newsData.length > 0 ? (
-          <>
-            <NewsCards>
-              {newsData.slice(startIndex, endIndex).map((newsCard, index) => (
-                <React.Fragment key={index}>
-                  <NewsCardComponent cardData={newsCard} />
-                </React.Fragment>
-              ))}
-            </NewsCards>
-            <Pagination
-              itemsData={newsData}
-              itemsPerPage={newsPerPage}
-              page={page}
-              handleChangePage={handleChangePage}
-              handleChangeItemsPerPage={handleChangeNewsPerPage}
-
-            />
-          </>
         ) : (
-          <NoDataParagraph>No data found</NoDataParagraph>
+          <>
+            <InfiniteScroll
+              dataLength={newsData.length}
+              next={fetchMoreNews}
+              hasMore={hasMore}
+              loader={<LoadingComponent loaderType="box" />}
+              endMessage={<p>No more news to load</p>}
+              scrollableTarget="root"
+            >
+              <NewsCards>
+                {newsData.map((newsCard, index) => (
+                  <React.Fragment key={index}>
+                    <NewsCardComponent cardData={newsCard} />
+                  </React.Fragment>
+                ))}
+              </NewsCards>
+            </InfiniteScroll>
+          </>
         )}
       </Section>
     </PressPageWrapper>
