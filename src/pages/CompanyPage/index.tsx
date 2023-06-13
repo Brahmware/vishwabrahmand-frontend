84ad7997523e38a8409ef34c4a0745652c270c2b
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-06-13 18:00:16 +0200

    refactor spaces

--- src/pages/CompanyPage/index.tsx
+++ src/pages/CompanyPage/index.tsx
@@ -5,25 +5,25 @@ import AlliesSection from './Allies';
 import KeyDocumentsSection from './KeyDocuments';
 
 const CompanyPageWrapper = styled(Box)(({ theme }) => ({
-    ...theme.bodyProps,
-    width: '100%',
-    padding: `${theme.customPadding.xl} ${theme.customPadding.sm}`,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'start',
-    justifyContent: 'center',
-    gap: theme.customSpaces.xl
+  ...theme.bodyProps,
+  width: '100%',
+  padding: `${theme.customPadding.xl} ${theme.customPadding.sm}`,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'start',
+  justifyContent: 'center',
+  gap: theme.customSpaces.xl
 }));
 
 const CompanyPage = () => {
-    return (
-        <CompanyPageWrapper>
-            <AboutSection />
-            <LeadershipSection />
-            <AlliesSection />
-            <KeyDocumentsSection />
-        </CompanyPageWrapper>
-    )
+  return (
+    <CompanyPageWrapper>
+      <AboutSection />
+      <LeadershipSection />
+      <AlliesSection />
+      <KeyDocumentsSection />
+    </CompanyPageWrapper>
+  )
 };
 
 export default CompanyPage
